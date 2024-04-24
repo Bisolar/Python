d02b659c94c87d963bd9f348b7c23b015362d7b9
Abisola Aransiola <ladipoabisola@gmail.com>
2024-04-24 09:36:05 +0100

    updated

--- cargame.py
+++ cargame.py
@@ -1,17 +1,14 @@
 # car game with navigation keys
+print("Welcome to your car race!! ")
 
 while True:
-    print("Welcome to your car race!! ")
     gamer_input = str(input("Select Navigation keys to move\nL = Left \nR = Right\nF = Forward\nB = backward\n >>")).upper()
     if gamer_input == "L":
         print("←←←←←")
-        break
     elif gamer_input == "R":
         print("➜➜➜➜➜")
-        break
     elif gamer_input == "F":
         print("↑↑↑↑↑")
-        break
     elif gamer_input == "B":
         print("↓↓↓↓↓")
         break
