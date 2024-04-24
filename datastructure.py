aeaf5e5ee4cef5dbc2734de8f15f81e428d6e806
Abisola Aransiola <ladipoabisola@gmail.com>
2024-04-24 21:57:16 +0100

    the

---
+++ datastructure.py
@@ -0,0 +1,12 @@
+# create a list of vowels using the list constructor and print them out individually
+
+# vowel = ["a", "e", "i", "o", "u"]
+
+# for v in vowel:
+#     print(v)
+
+# Assignment create a program that check if a word is has vowel
+# collect input from user to check if the word has a vowel
+
+user_input = str(input("Enter a word\n>> "))
+
